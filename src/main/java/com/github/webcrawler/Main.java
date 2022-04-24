efed0cda2c78478dbdacaddcafbfc4eebc5e32ce
Felix Hilgers <felix_hilgers@posteo.de>
2022-04-24 02:43:21 +0200

    feat: implement main

--- src/main/java/com/github/webcrawler/Main.java
+++ src/main/java/com/github/webcrawler/Main.java
@@ -1,5 +1,11 @@
 package com.github.webcrawler;
 
+import com.github.webcrawler.commandline.PicocliApp;
+
 public class Main {
-    public static void main(String[] args) {}
+  public static void main(String[] args) {
+
+    int exitCode = new PicocliApp().execute(args);
+    System.exit(exitCode);
+  }
 }
